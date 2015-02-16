5bce156a93e2c0645da252acdc4c6c03dfde63f3
Mr. Benito <bsegovia@gmail.com>
2015-02-16 00:49:07 -0600

    need progress endpoint

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -61,6 +61,11 @@ class TopicsController < ApplicationController
     end
   end
 
+  def list_all
+    
+  end
+  
+
   private
     # Use callbacks to share common setup or constraints between actions.
     def set_topic
